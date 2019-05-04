b08e413b307f92e9a33b781ade690c2e625cfda4
Sean DMR <sean@flyinggrizzly.net>
2019-05-04 19:01:12 +0100

    Validate method and attribute names

--- spec/rstruct/index.spec.js
+++ spec/rstruct/index.spec.js
@@ -1,12 +1,51 @@
 import RStruct from 'rstruct'
 
 describe('RStruct', () => {
-  it('assigns attribute names and attributes', () => {
-    let Struct = new RStruct('a', 'b')
-    let instance = new Struct(1, 2)
+  describe('setting attributes', () => {
+    it('assigns attribute names and attributes', () => {
+      let Struct = new RStruct('a', 'b')
+      let instance = new Struct(1, 2)
 
-    expect(instance.a).toEqual(1)
-    expect(instance.b).toEqual(2)
+      expect(instance.a).toEqual(1)
+      expect(instance.b).toEqual(2)
+    })
+
+    describe('attribute and method name validation', () => {
+      it('requires a char or underscore as the first character', () => {
+        expect(() => {
+          new RStruct('1stParam')
+        }).toThrowError()
+
+        expect(() => {
+          new RStruct('_privateParam', 'publicParam', 'PascalParam')
+        }).not.toThrowError()
+
+        expect(() => {
+          new RStruct('a', {
+            $method() {
+              return `
+                '$method' is a valid identifier, invalid RStruct method name.
+                See https://developer.mozilla.org/en-US/docs/Glossary/Identifier for more.
+              `
+            }
+          }).toThrowError()
+        })
+      })
+
+      it('allows numbers in names after the first character', () => {
+        expect(() => {
+          new RStruct('paramWithNumeral1inIt')
+        }).not.toThrowError()
+
+        expect(() => {
+          new RStruct('a', {
+            methodWithNumeral1InName() {
+              return 'valid'
+            }
+          })
+        }).not.toThrowError()
+      })
+    })
   })
 
   describe('when given an object with method definitions', () => {
--- src/rstruct/index.js
+++ src/rstruct/index.js
@@ -2,13 +2,17 @@ import _ from 'lodash'
 
 export default function RStruct(...attributes) {
   let methods
-  if (_.last(attributes) instanceof Object)
+  if (_.last(attributes) instanceof Object) {
     methods = attributes.pop()
+  }
+
+  attributes.forEach(name => {
+    validateAttributeName(name)
+  })
 
   const klass = class {
     constructor() {
       attributes.forEach((name, index) => {
-        //validateAttributeName(name)
         this[name] = arguments[index]
       })
     }
@@ -18,6 +22,8 @@ export default function RStruct(...attributes) {
     let methodNames = _.keys(methods)
 
     methodNames.forEach(name => {
+      validateMethodName(name)
+
       let methodBody = methods[name]
 
       if (!(methodBody instanceof Function))
@@ -28,4 +34,34 @@ export default function RStruct(...attributes) {
   }
 
   return klass
+}
+
+function validateAttributeName(name) {
+  validateAllCharsOfName(name)
+  validateFirstCharOfName(name)
+}
+
+function validateMethodName(name) {
+  // JS will raise a SyntaxError if the first char of an object
+  // key is a numeral, so we're covered there for method names
+  // starting with numbers
+  validateAllCharsOfName(name)
+}
+
+function validateAllCharsOfName(name) {
+  const validCharacters = "abcdefghijklmnopqrstuvwxyz_1234567890".split('')
+  let characters = name.split('')
+
+  characters.forEach(c => {
+    if (!(_.includes(validCharacters, _.toLower(c))))
+      throw new Error(`${name}: attributes and method names must consist of only chars, numbers, and underscores ('_')`)
+  })
+}
+
+function validateFirstCharOfName(name) {
+  const validCharacters = "abcdefghijklmnopqrstuvwxyz_".split('')
+  let firstCharacter = _.head(name.split(''))
+
+  if (!(_.includes(validCharacters, _.toLower(firstCharacter))))
+    throw new Error(`${name}: attribute and method names must begin with a char or underscore ('_')`)
 }

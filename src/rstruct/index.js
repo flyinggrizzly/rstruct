7af214d7252c5f4d6d5c77d149f716993627f61a
Sean DMR <sean@flyinggrizzly.net>
2019-08-02 09:20:13 +0100

    Validate method names

--- src/rstruct/index.js
+++ src/rstruct/index.js
@@ -19,6 +19,8 @@ const RStruct = function (...attributes) {
   if (methods) {
     let methodNames = Object.keys(methods)
 
+    validateBarewordNames(methodNames)
+
     methodNames.forEach(name => {
       let methodBody = methods[name]
 
